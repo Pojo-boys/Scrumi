02f464e1c823910da4fd4ddb521ff5d9454860f2
guyfredw <guyfredw@gmail.com>
2020-11-30 12:15:45 -0500

    Fixed issue with checkboxes Co-authored-by: Andrew a_crook@icloud.com Co-authored-by: Kevin kdoxsey@gmail.com Co-authored-by: Elijah elijahdavison2@gmail.com

--- src/components/shared/TaskForm.js
+++ src/components/shared/TaskForm.js
@@ -3,44 +3,47 @@ import React from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 
-const TaskForm = ({ task, handleSubmit, handleChange }) => (
-  <form onSubmit={handleSubmit}>
-    <Form.Group controlId='task'>
-      <Form.Label>Title</Form.Label>
-      <Form.Control
-        required
-        type='text'
-        placeholder="Task title here"
-        value={task.title}
-        name="title"
-        onChange={handleChange}
-      />
+const TaskForm = ({ task, handleSubmit, handleChange }) => {
+  return (
+    <form onSubmit={handleSubmit}>
+      <Form.Group controlId='task'>
+        <Form.Label>Title</Form.Label>
+        <Form.Control
+          required
+          type='text'
+          placeholder="Task title here"
+          value={task.title}
+          name="title"
+          onChange={handleChange}
+        />
 
-      <Form.Label>Description</Form.Label>
-      <Form.Control
-        required
-        type='text'
-        placeholder="Description of task here"
-        value={task.description}
-        name="description"
-        onChange={handleChange}
-      />
+        <Form.Label>Description</Form.Label>
+        <Form.Control
+          required
+          type='text'
+          placeholder="Description of task here"
+          value={task.description}
+          name="description"
+          onChange={handleChange}
+        />
 
-      <Form.Label>Completed</Form.Label>
-      <Form.Control
-        type="checkbox"
-        value={task.isChecked}
-        name="isChecked"
-        onChange={handleChange}
-      />
-    </Form.Group>
-    <Button
-      variant="primary"
-      type="submit"
-    >
-      Submit
-    </Button>
-  </form>
-)
+        <Form.Label>Completed</Form.Label>
+        <Form.Control
+          type="checkbox"
+          // value={task.isChecked}
+          checked={task.isChecked}
+          name="isChecked"
+          onChange={handleChange}
+        />
+      </Form.Group>
+      <Button
+        variant="primary"
+        type="submit"
+      >
+        Submit
+      </Button>
+    </form>
+  )
+}
 
 export default TaskForm
