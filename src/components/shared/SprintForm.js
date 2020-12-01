300923de4c4c84a758567e2e950dc261fba11796
kdoxsey <kdoxse@gmail.com>
2020-12-01 17:45:23 -0500

    Fixed linter errors & made sprint routes Co-authored-by: Andrew a_crook@icloud.com Co-authored-by: Guy guyfredw@gmail.com

--- src/components/shared/SprintForm.js
+++ src/components/shared/SprintForm.js
@@ -4,40 +4,40 @@ import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 
 const SprintForm = ({ sprint, handleSubmit, handleChange, match }) => {
-    return (
-      <form onSubmit={handleSubmit}>
-        <Form.Group controlId='sprint'>
-          <Form.Label>Sprint Name</Form.Label>
-          <Form.Control
-            required
-            type='text'
-            placeholder="Sprint name here"
-            value={sprint.name}
-            name="name"
-            onChange={handleChange}
-          />
+  return (
+    <form onSubmit={handleSubmit}>
+      <Form.Group controlId='sprint'>
+        <Form.Label>Sprint Name</Form.Label>
+        <Form.Control
+          required
+          type='text'
+          placeholder="Sprint name here"
+          value={sprint.name}
+          name="name"
+          onChange={handleChange}
+        />
 
-          <Form.Label>Completion Timeframe</Form.Label>
-          <Form.Control as='select'
-            value={sprint.timeframe}
-            name="timeframe"
-            onChange={handleChange}
-          >
-						<option>1</option>
-						<option>2</option>
-						<option>3</option>
-						<option>4</option>
-					</Form.Control>
-        </Form.Group>
-        <Button
-          className="form-submit-button"
-          variant="primary"
-          type="submit"
+        <Form.Label>Completion Timeframe</Form.Label>
+        <Form.Control as='select'
+          value={sprint.timeframe}
+          name="timeframe"
+          onChange={handleChange}
         >
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+        </Form.Control>
+      </Form.Group>
+      <Button
+        className="form-submit-button"
+        variant="primary"
+        type="submit"
+      >
         Submit
-        </Button>
-      </form>
-    )
-  }
-	
-	export default SprintForm
+      </Button>
+    </form>
+  )
+}
+
+export default SprintForm
